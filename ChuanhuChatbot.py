e744cf1ed05df81f97e960de3e33016136e32e39
Alauda95 <50646749+Alauda95@users.noreply.github.com>
2023-04-05 18:57:47 +0800

    Update ChuanhuChatbot.py

--- ChuanhuChatbot.py
+++ ChuanhuChatbot.py
@@ -96,7 +96,7 @@ with gr.Blocks(css=customCSS, theme=small_and_beautiful_theme) as demo:
                     keyTxt = gr.Textbox(
                         show_label=True,
                         placeholder=f"OpenAI API-key...",
-                        value=hide_middle_chars(my_api_key),
+                        value="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",
                         type="password",
                         visible=not HIDE_MY_KEY,
                         label="API-Key",
